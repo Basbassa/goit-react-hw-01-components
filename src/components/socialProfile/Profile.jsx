6a59fefffe751021518bc16dd2eb0d91153ba284
Anna <basbassa@gmail.com>
2023-06-08 20:03:42 +0200

    working on css profile

--- src/components/socialProfile/Profile.jsx
+++ src/components/socialProfile/Profile.jsx
@@ -10,21 +10,21 @@ function Profile() {
           alt="User avatar"
           className="avatar"
         />
-        <p className="name">Petra Marica</p>
-        <p className="tag">@pmarica</p>
-        <p className="location">Salvador, Brasil</p>
+        <p className="name secondaryDescription">Petra Marica</p>
+        <p className="tag secondaryDescription">@pmarica</p>
+        <p className="location secondaryDescription">Salvador, Brasil</p>
       </div>
 
       <ul className="stats">
-        <li>
+        <li className="secondaryDescription">
           <span className="label">Followers</span>
           <span className="quantity">1000</span>
         </li>
-        <li>
+        <li className="secondaryDescription">
           <span className="label">Views</span>
           <span className="quantity">2000</span>
         </li>
-        <li>
+        <li className="secondaryDescription">
           <span className="label">Likes</span>
           <span className="quantity">3000</span>
         </li>
