1162c02998faff9563dbd3a68e3c69777b4d3e53
Anna <basbassa@gmail.com>
2023-06-11 20:20:11 +0200

    much better css friendsListr

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -7,20 +7,19 @@ const FriendList = () => {
     <ul className={styles['friend-list']}>
       {friendsData.map(friend => (
         <li className={styles.item} key={friend.id}>
-          <span
-            className={`status ${friend.isOnline ? 'online' : 'offline'}`}
-          ></span>
-          <img
-            className={styles.avatar}
-            src={friend.avatar}
-            alt="User avatar"
-            width="48"
-          />
-          <div
-            className={`${styles['status-dot']} ${
-              friend.isOnline ? 'online' : 'offline'
-            }`}
-          ></div>
+          {friend.isOnline ? (
+            <div className={`${styles['status-dot']} ${styles.online}`}></div>
+          ) : (
+            <div className={`${styles['status-dot']} ${styles.offline}`}></div>
+          )}
+          <div className={styles['avatar-container']}>
+            <img
+              className={styles.avatar}
+              src={friend.avatar}
+              alt="User avatar"
+              width="48"
+            />
+          </div>
           <p className={styles.name}>{friend.name}</p>
         </li>
       ))}
