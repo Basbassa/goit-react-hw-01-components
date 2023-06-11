6dd1aa63bdad3acc6d245ef8c2ad7ff9d4c77f6d
Anna <basbassa@gmail.com>
2023-06-11 20:07:34 +0200

    better css FriendsList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import friendsData from './friends.json';
+import styles from './Lists.module.css';
 
 const FriendList = () => {
   return (
-    <ul className="friend-list">
+    <ul className={styles['friend-list']}>
       {friendsData.map(friend => (
-        <li className="item" key={friend.id}>
+        <li className={styles.item} key={friend.id}>
           <span
             className={`status ${friend.isOnline ? 'online' : 'offline'}`}
           ></span>
           <img
-            className="avatar"
+            className={styles.avatar}
             src={friend.avatar}
             alt="User avatar"
             width="48"
           />
-          <p className="name">{friend.name}</p>
+          <div
+            className={`${styles['status-dot']} ${
+              friend.isOnline ? 'online' : 'offline'
+            }`}
+          ></div>
+          <p className={styles.name}>{friend.name}</p>
         </li>
       ))}
     </ul>
